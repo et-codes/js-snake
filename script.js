a9aac8935a117d2eec75ece7311be8def82fa89d
Eric Thornton <ericthornton43@gmail.com>
2022-08-27 15:17:19 -0600

    First working version.

--- script.js
+++ script.js
@@ -1,7 +1,158 @@
-const canvas = document.getElementById('canvas')
-const ctx = canvas.getContext('2d');
-canvas.height = 600;
-canvas.width = 800;
+// Initialize constants and variables
+const SIZE = 20;
+const WIDTH = 800;
+const HEIGHT = 600;
+const MAX_X = WIDTH / SIZE;
+const MAX_Y = HEIGHT / SIZE;
+const FPS = 10;
+let score = 0;
+let highScore = 0;
+let intervalId;
+let gameOver = false;
 
-ctx.fillStyle = 'lightgreen';
-ctx.fillRect(0, 0, canvas.width, canvas.height);
+// Set up the canvas
+const canvasElement = document.getElementById('canvas')
+canvasElement.width = WIDTH;
+canvasElement.height = HEIGHT;
+const canvas = canvasElement.getContext('2d');
+
+// Create snake and apple objects
+const snake = [{
+  x: Math.floor(WIDTH / SIZE / 2),
+  y: Math.floor(HEIGHT / SIZE / 2),
+  dx: 0,
+  dy: 0
+}];
+const head = snake[0];
+const apple = { x: 0, y: 0 };
+
+const getApplePosition = () => {
+  apple.x = Math.floor(Math.random() * WIDTH / SIZE);
+  apple.y = Math.floor(Math.random() * HEIGHT / SIZE);
+  if (apple.x === head.x && apple.y === head.y) getApplePosition();
+}
+
+const drawCanvas = () => {
+  // Draw background
+  canvas.fillStyle = 'lightgreen';
+  canvas.fillRect(0, 0, WIDTH, HEIGHT);
+
+  // Draw snake
+  let green = 100;
+  snake.forEach(segment => {
+    canvas.fillStyle = `rgb(0, ${green}, 0)`;
+    canvas.fillRect(segment.x * SIZE, segment.y * SIZE, SIZE, SIZE);
+    green = green === 100 ? 150 : 100;
+  });
+
+  // Draw apple
+  canvas.fillStyle = 'darkred';
+  canvas.fillRect(apple.x * SIZE, apple.y * SIZE, SIZE, SIZE);
+
+  // Render GAME OVER text if necessary
+  if (gameOver) renderGameOver();
+}
+
+const handleKeys = (event) => {
+  switch (event.key) {
+    case 'ArrowUp':
+      head.dy = -1;
+      head.dx = 0;
+      break;
+    case 'ArrowDown':
+      head.dy = 1;
+      head.dx = 0;
+      break;
+    case 'ArrowLeft':
+      head.dx = -1;
+      head.dy = 0;
+      break;
+    case 'ArrowRight':
+      head.dx = 1;
+      head.dy = 0;
+      break;
+  }
+}
+
+const moveSnake = () => {
+  const oldPosition = { x: head.x, y: head.y };
+  head.x += head.dx;
+  head.y += head.dy;
+
+  for (i = 1; i < snake.length; i++) {
+    const olderPosition = { x: snake[i].x, y: snake[i].y };
+    snake[i].x = oldPosition.x;
+    snake[i].y = oldPosition.y;
+    oldPosition.x = olderPosition.x;
+    oldPosition.y = olderPosition.y;
+  }
+}
+
+const checkCollisions = () => {
+  // Check if off the canvas
+  if (head.x < 0 || head.x >= MAX_X || head.y < 0 || head.y >= MAX_Y) {
+    endGame();
+    return;
+  }
+
+  // Check if collided with self
+  for (let i = 1; i < snake.length; i++) {
+    if (head.x === snake[i].x && head.y === snake[i].y) {
+      endGame();
+      return;
+    }
+  }
+
+  // Check if collided with apple
+  if (head.x === apple.x && head.y === apple.y) {
+    score++;
+    snake.push({ x: head.x, y: head.y });
+    getApplePosition();
+  }
+}
+
+const endGame = () => {
+  clearInterval(intervalId);
+  gameOver = true;
+}
+
+const renderGameOver = () => {
+  canvas.fillStyle = 'black';
+  canvas.font = '100px sans-serif';
+  canvas.textAlign = 'center';
+  canvas.textBaseline = 'middle';
+  canvas.fillText('GAME OVER', WIDTH / 2, HEIGHT / 2);
+}
+
+const updateScore = () => {
+  if (score > highScore) highScore = score;
+  document.getElementById('score').textContent = `Score: ${score}`;
+  document.getElementById('high-score').textContent = `High Score: ${highScore}`;
+}
+
+const resetGame = () => {
+  score = 0;
+  snake.splice(1);
+  head.x = Math.floor(WIDTH / SIZE / 2);
+  head.y = Math.floor(HEIGHT / SIZE / 2);
+  head.dx = 0;
+  head.dy = 0;
+  gameOver = false;
+  startGame();
+}
+
+const loopGame = () => {
+  moveSnake();
+  checkCollisions();
+  drawCanvas();
+  updateScore();
+}
+
+const startGame = () => {
+  getApplePosition();
+  window.addEventListener('keydown', handleKeys);
+  document.getElementById('play-again').addEventListener('click', resetGame);
+  intervalId = setInterval(loopGame, (1000 / FPS));
+}
+
+startGame();
